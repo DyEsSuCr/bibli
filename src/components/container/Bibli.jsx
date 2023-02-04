e384e41bac37d37eb854c4d748addc70054845fb
DylanSuarez <2000sajo@gmail.com>
2023-02-04 11:03:33 -0500

    generate localstorage

--- src/components/container/Bibli.jsx
+++ src/components/container/Bibli.jsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from 'react'
+
 export function Blibli() {
+  const [gallery, setGallery] = useState([])
+
+  const isLocalGallery = JSON.parse(localStorage.getItem('gallery'))
+
+  // NOTE: Crea El LocalStorage
+  useEffect(() => {
+    if (!isLocalGallery) {
+      localStorage.setItem('gallery', JSON.stringify(gallery))
+    } else {
+      setGallery(isLocalGallery)
+    }
+  }, [])
+
   return (
     <div className="min-w-full min-h-screen">
       <h1>BIBLI</h1>
