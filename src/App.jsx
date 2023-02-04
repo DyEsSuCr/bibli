d22747bba76ebe73335e6342950bf661c5506747
DylanSuarez <2000sajo@gmail.com>
2023-02-04 10:54:59 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Blibli } from './components/container/Bibli'
+
+function App() {
+  return (
+    <>
+      <Blibli />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/container/Bibli.jsx
@@ -0,0 +1,7 @@
+export function Blibli() {
+  return (
+    <div className="min-w-full min-h-screen">
+      <h1>BIBLI</h1>
+    </div>
+  )
+}
